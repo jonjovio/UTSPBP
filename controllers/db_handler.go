caf713d2f117ad824ec00231e5a4b93b20cfdbe7
jonjovio <if-21038@students.ithb.ac.id>
2024-03-08 13:07:40 +0700

    Awalan

---
+++ controllers/db_handler.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"database/sql"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func connect() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(localhost:3306)/db_latihan_pbp?parseTime=true&loc=Asia%2FJakarta")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func connectgorm() *gorm.DB {
+	dsn := "root:@tcp(localhost:3306)/db_latihan_pbp?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
